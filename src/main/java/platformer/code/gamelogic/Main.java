5480209f25f2712510bee72e46dc03da93c6be9b
Munkh26 <munkhbatmunkhsoyombo@gmail.com>
2026-06-15 15:03:41 +0000

    map

--- src/main/java/platformer/code/gamelogic/Main.java
+++ src/main/java/platformer/code/gamelogic/Main.java
@@ -41,12 +41,12 @@ public class Main extends GameBase implements PlayerDieListener, PlayerWinListen
 	public void init() {
 		GameResources.load();
 
-		currentLevelIndex = 0;
+		currentLevelIndex = 1;
 
 		levels = new LevelData[5];
 		try {
 			levels[0] = LeveldataLoader.loadLeveldata("src/main/java/platformer/maps/map1.txt");
-			levels[1] = LeveldataLoader.loadLeveldata("src/main/java/platformer/maps/waterMap.txt");
+			levels[1] = LeveldataLoader.loadLeveldata("src/main/java/platformer/maps/customLevel.txt");
 			levels[2] = LeveldataLoader.loadLeveldata("src/main/java/platformer/maps/gasTest1.txt");
 			levels[3] = LeveldataLoader.loadLeveldata("src/main/java/platformer/maps/gasTest2.txt");
 		} catch (Exception e) {
